2a3fac24d425ad27e701d0759b2755073a9eac48
vegevega <131756114+vegevega@users.noreply.github.com>
2023-05-03 14:16:45 +0800

    Add files via upload

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -12,11 +12,11 @@
                         <span>学生入口</span>
                     </template>
 
-                    <el-menu-item index="1-1"><i class="el-icon-edit-outline"></i>个人成绩</el-menu-item>
-                    <el-menu-item index="1-2"><i class="el-icon-edit-outline"></i>志愿服务</el-menu-item>
+                    <el-menu-item index="1-1" @click="$router.push('/student/GPA')"><i class="el-icon-edit-outline"></i>个人成绩</el-menu-item>
+                    <el-menu-item index="1-2" @click="$router.push('/student/volunteer-service')"><i class="el-icon-edit-outline"></i>志愿服务</el-menu-item>
                     <el-menu-item index="1-3" @click="$router.push('/student/research')"><i class="el-icon-edit-outline"></i>科研情况</el-menu-item>
-                    <el-menu-item index="1-4"><i class="el-icon-edit-outline"></i>社会实践</el-menu-item>
-                    <el-menu-item index="1-5"><i class="el-icon-edit-outline"></i>学生骨干服务岗位</el-menu-item>
+                    <el-menu-item index="1-4" @click="$router.push('/student/social')"><i class="el-icon-edit-outline"></i>社会实践</el-menu-item>
+                    <el-menu-item index="1-5" @click="$router.push('/student/service')"><i class="el-icon-edit-outline"></i>学生骨干服务岗位</el-menu-item>
                     <el-menu-item index="1-6" @click="$router.push('/student/personal-summary')"><i class="el-icon-edit-outline"></i>个人学年总结</el-menu-item>
 
                 </el-submenu>
@@ -28,11 +28,11 @@
 
                     <el-submenu index="2-1">
                         <template slot="title"><i class="el-icon-edit-outline"></i>个人成绩</template>
-                        <el-menu-item index="2-1-1">个人成绩审核</el-menu-item>
+                        <el-menu-item index="2-1-1" @click="$router.push('/judge/GPA')">学生成绩导入</el-menu-item>
                     </el-submenu>
                     <el-submenu index="2-2">
                         <template slot="title"><i class="el-icon-edit-outline"></i>志愿服务</template>
-                        <el-menu-item index="2-2-1">志愿服务导入</el-menu-item>
+                        <el-menu-item index="2-2-1" @click="$router.push('/judge/volunteer-service')">志愿服务时长导入</el-menu-item>
                     </el-submenu>
 
                     <el-submenu index="2-3">
@@ -41,11 +41,11 @@
                     </el-submenu>
                     <el-submenu index="2-4">
                         <template slot="title"><i class="el-icon-edit-outline"></i>社会实践</template>
-                        <el-menu-item index="2-4-1">社会实践打分</el-menu-item>
+                        <el-menu-item index="2-4-1" @click="$router.push('/judge/social')">社会实践打分</el-menu-item>
                     </el-submenu>
                     <el-submenu index="2-5">
                         <template slot="title"><i class="el-icon-edit-outline"></i>学生骨干服务岗位</template>
-                        <el-menu-item index="2-5-1">学生骨干服务岗位打分</el-menu-item>
+                        <el-menu-item index="2-5-1" @click="$router.push('/judge/service')">学生骨干服务岗位打分</el-menu-item>
                     </el-submenu>
                     <el-submenu index="2-6">
                         <template slot="title"><i class="el-icon-edit-outline"></i>个人学年总结</template>
@@ -94,7 +94,8 @@
 .main-content {
     flex: 1;
     padding: 20px;
-}
+    min-height:100vh;
+}   
 </style>
 
 <script>
