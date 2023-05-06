2060d2d19ea7d693637f2a16dd52b7851334cfcf
yaoyaolili <104288477+yaoyaolili@users.noreply.github.com>
2023-05-06 16:04:49 +0800

    Update ServiceUpload.vue

--- src/views/students/ServiceUpload.vue
+++ src/views/students/ServiceUpload.vue
@@ -6,7 +6,7 @@
       <div v-for="(project, index) in form.research" :key="project.key">
         <el-row type="flex">
           <el-col :span="22">
-            <h4 style="float: left;">{{ '参与的服务项目' + (index + 1) }} </h4>
+            <h4 style="float: left;">{{ '参与的服务项目' + (index + 1) }}</h4>
           </el-col>
           <!-- 删除按钮 -->
           <el-col :span="2" style="display:flex;justify-content:flex-end;align-items:center;">
@@ -15,52 +15,47 @@
         </el-row>
         <el-row>
           <el-col :span="20">
-            <el-form-item label="名称">
-              <el-input placeholder="请输入服务项目名称" v-model="form.research[index].name">
-              </el-input>
+            <el-form-item label="名称" prop="name" >
+              <el-input placeholder="请输入服务岗位名称" v-model="form.name"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="20">
-            <el-form-item label="负责人">
-              <el-input placeholder="请输入服务项目负责人" v-model="form.research[index].manager">
-              </el-input>
+            <el-form-item label="岗位负责人" prop="manager" >
+              <el-input placeholder="请输入服务岗位负责人" v-model="form.manager"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="20">
-            <el-form-item label="负责单位">
-              <el-input placeholder="请输入服务项目负责单位" v-model="form.research[index].organization">
-              </el-input>
+            <el-form-item label="岗位负责单位" prop="organization" >
+              <el-input placeholder="请输入服务岗位负责单位" v-model="form.organization"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="20">
-            <el-form-item label="内容">
-              <el-input placeholder="请输入服务内容" v-model="form.research[index].content"></el-input>
+            <el-form-item label="岗位职责" prop="content" >
+              <el-input placeholder="请输入岗位职责" v-model="form.content"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="20">
-            <el-form-item label="成果">
-              <el-input placeholder="请输入服务成果" v-model="form.research[index].achievements"></el-input>
+            <el-form-item label="成果" prop="achievements" >
+              <el-input placeholder="请输入在岗期间成果" v-model="form.achievements"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="20">
-            <el-form-item label="时长">
-              <el-input placeholder="请输入服务时长" v-model="form.research[index].time"></el-input>
+            <el-form-item label="时长" prop="time" >
+              <el-input placeholder="请输入在岗服务时长" v-model="form.time"></el-input>
             </el-form-item>
           </el-col>
-
         </el-row>
-
       </div>
       <el-form-item style="margin-top: 20px">
-        <el-button type="warning" @click="addProject">增加社会实践项目</el-button>
+        <el-button type="warning" @click="addProject">增加服务项目</el-button>
         <el-button type="primary" @click="submitForm">提交</el-button>
         <el-button @click="clearForm">重置</el-button>
       </el-form-item>
@@ -82,28 +77,43 @@ export default {
             content: '',
             achievements: '',
             time: ''
-
           }
         ]
       },
       rules: {
-        studySummary: [
-          { required: true, message: '该项不能为空', trigger: 'blur' },
-        ],
-        practiceSummary: [
-          { required: true, message: '该项不能为空', trigger: 'blur' },
-        ],
-        selfEvaluation: [
-          { required: true, message: '该项不能为空', trigger: 'blur' },
-        ],
-      }
+
+            name: [
+              {required: true, message: '该项不能为空', trigger: 'blur'},
+            ],
+            manager: [
+              {required: true, message: '该项不能为空', trigger: 'blur'},
+            ],
+            organization: [
+              {required: true, message: '该项不能为空', trigger: 'blur'},
+            ],
+            content: [
+              {required: true, message: '该项不能为空', trigger: 'blur'},
+            ],
+            time: [
+              {required: true, message: '该项不能为空', trigger: 'blur'},
+            ],
+            achievements: [
+              {required: true, message: '该项不能为空', trigger: 'blur'},
+            ],
+          }
+
+
     };
+
+
   },
   methods: {
+
     submitForm() {
       this.$refs.form.validate((valid) => {
         if (valid) {
           // 将表单数据传送到后台服务器
+          console.log('submitForm called')
           console.log(this.form);
           // 在提交成功后进行一些提示或跳转
           this.$message.success('提交成功');
@@ -127,17 +137,14 @@ export default {
         time: '',
         key: Date.now()
       });
-      console.log(this.form);
     },
     removeProject(project) {
       var index = this.form.research.indexOf(project)
       if (index !== -1) {
         this.form.research.splice(index, 1)
       }
-      console.log(this.form);
-
     }
-  },
+  }
 };
 </script>
 
