c75e32cb66c81cbfef69fb1efb1520453fc268c7
dumplant <2437789306@qq.com>
2023-05-16 21:27:16 +0800

    add 【科研、个人总结】上传到后端

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
 
-
 const service = axios.create({
-  baseURL: 'http://localhost:8080/admin-api',
-  timeout: 5000000
+  baseURL: 'http://localhost:28080/api/',
+  timeout: 5000000,
 });
 
 service.interceptors.request.use(
-    // 函数
-  config => {
-    
-  },
-  error => {
+  // 函数
+  (config) => {},
+  (error) => {
     // 对请求错误做些什么
     console.log(error);
     return Promise.reject(error);
@@ -19,13 +16,13 @@ service.interceptors.request.use(
 );
 
 service.interceptors.response.use(
-  response => {
+  (response) => {
     // 对响应数据进行处理
     // ...
-    
+
     return response.data;
   },
-  error => {
+  (error) => {
     // 对响应错误做些什么
     console.log(error);
     return Promise.reject(error);
@@ -33,21 +30,21 @@ service.interceptors.response.use(
 );
 
 export const saveExcelData = async (file) => {
-    try {
-      const formData = new FormData();
-      formData.append('file', file);
-  
-      const response = await axios.post('/api/grade/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-  
-      return response.data;
-    } catch (error) {
-      console.error(error);
-      throw new Error('保存 Excel 文件失败');
-    }
-  };
+  try {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const response = await axios.post('/api/grade/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('保存 Excel 文件失败');
+  }
+};
 
 export default service;
