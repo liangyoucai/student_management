4753e3f34295f1d947c53b470da8a479bfb0c2b7
wd-v <131756114+wd-v@users.noreply.github.com>
2023-05-21 19:56:47 +0800

    学生端GPA、志愿服务时长显示（stuID待修改）

--- src/views/students/StudentGPA.vue
+++ src/views/students/StudentGPA.vue
@@ -8,85 +8,52 @@
 
       <div class="main">
         <el-table :data="tableData" style="width: 100%" id="mainTable" max-height="500">
-          <el-table-column prop="no" label="序号" width="80"> 
-            <template slot-scope="scope">
-              {{ scope.$index + 1}}
-            </template>
-          </el-table-column>
-          <el-table-column prop="date" label="更新日期" width="140"> </el-table-column>
-          <el-table-column prop="ID" label="学号" width="140"></el-table-column>
-          <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-          <el-table-column prop="class" label="学苑" width="120"></el-table-column>
-          <el-table-column prop="GPA" label="GPA" width="120"></el-table-column>
+          <!-- <el-table-column prop="date" label="更新日期" width="140"> </el-table-column> -->
+          <el-table-column prop="stuNum" label="学号" width="140"></el-table-column>
+          <el-table-column prop="stuName" label="姓名" width="120"></el-table-column>
+          <el-table-column prop="gpa" label="GPA" width="120"></el-table-column>
         </el-table>
       </div>
   
-      <div class="footer">
-        <el-button type="primary" v-if="state!=1" @click="confirm(); showForm = false">确认</el-button>
-        <el-button type="danger" v-if="state!=1" @click="showForm = true">信息有误</el-button>
-          <el-form v-if="showForm" :rules="formRules" ref="form" label-width="120px" :model="formData">
-            <el-form-item label="问题描述" prop="description" style="margin-top: 50px;">
-              <el-input v-model="formData.description"></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="report">提交</el-button>
-            </el-form-item>
-          </el-form>
-        <el-button type="info" v-if="state==1" disabled>已确认</el-button>
-      </div>
     </div>
   </template>
   
   <script>
+  import axios from 'axios';
   import FileSaver from "file-saver";
   import * as XLSX from "xlsx";
   export default {
     data() {
       return {
-      showForm: false, // 控制是否显示表单
-      formData: {
-        description: "", // 表单数据
-      },
-      formRules: { // 表单验证规则
-        description: [
-          { required: true, message: "问题描述不能为空", trigger: "blur" },
+        tableData: [
+          {
+            // date: "2002-06-28",
+            // ID: "2200022758",
+            // name: "ZYY",
+            // class: "求知三苑",
+            // GPA: "4.00",
+            stuNum: "",
+            stuName: "",
+            gpa: "",
+          },
         ],
-      },
-      tableData: [
-        {
-          date: "2002-06-28",
-          ID: "2200022758",
-          name: "ZYY",
-          class: "求知三苑",
-          GPA: "4.00",
-        },
-      ],
-      state: 0, // 0-未确认，1-已确认，2-有误，目前默认未确认，写了后端以后可以从后端调用相应数据
       };
     },
-    
+    mounted() {
+      this.getStudentInfo();
+    },
     methods: {
-      confirm(){
-        this.state = 1;
-        // 将确认信息传送到后台服务器
-        console.log(state);
-        // 在提交成功后进行一些提示或跳转
-        this.$message.success('信息已确认');
-      },
-      report() {
-        this.$refs.form.validate((valid) => {
-          if (valid) {
-            // 表单验证通过，提交表单数据
-            console.log(this.formData);
-            this.state = 2;
-            this.$message.success("信息已上报，请等待老师与您联系");
-            this.showForm = false; // 隐藏表单
-          } else {
-            // 表单验证不通过，提示错误信息
-            this.$message.error("备注不能为空");
-          }
+      getStudentInfo() {
+      const stuId = 1; // 你需要替换为实际的学生ID
+      axios
+        .post(`http://localhost:28080/api/grade/${stuId}/get-grade-info`)
+        .then(response => {
+          this.tableData = [response.data.data];
+        })
+        .catch(error => {
+          console.error('获取学生信息失败', error);
         });
-      }
+      },
     },
   };
   </script>
--- src/views/students/StudentVolunteerService.vue
+++ src/views/students/StudentVolunteerService.vue
@@ -8,25 +8,18 @@
       <el-divider></el-divider>
       <div class="main">
         <el-table :data="tableData" style="width: 100%" id="mainTable" max-height="500">
-          <el-table-column prop="no" label="序号" width="80"> 
-            <template slot-scope="scope">
-              {{ scope.$index + 1}}
-            </template>
+          <!-- <el-table-column prop="date" label="更新日期" width="140"> </el-table-column> -->
+          <el-table-column prop="stuNum" label="学号" width="140">
           </el-table-column>
-          <el-table-column prop="date" label="更新日期" width="140"> </el-table-column>
-          <el-table-column prop="ID" label="学号" width="140">
+          <el-table-column prop="stuName" label="姓名" width="120">
           </el-table-column>
-          <el-table-column prop="name" label="姓名" width="120">
-          </el-table-column>
-          <el-table-column prop="class" label="学苑" width="120">
-          </el-table-column>
-          <el-table-column prop="hours" label="志愿服务时长" width="120">
+          <el-table-column prop="time" label="志愿服务时长" width="120">
           </el-table-column>
             
         </el-table>
       </div>
   
-      <div class="footer">
+      <!-- <div class="footer">
         <el-button type="primary" v-if="state!=1" @click="confirm(); showForm = false">确认</el-button>
         <el-button type="danger" v-if="state!=1" @click="showForm = true">信息有误</el-button>
           <el-form v-if="showForm" :rules="formRules" ref="form" label-width="120px" :model="formData">
@@ -38,62 +31,80 @@
             </el-form-item>
           </el-form>
         <el-button type="info" v-if="state==1" disabled>已确认</el-button>
-      </div>
+      </div> -->
 
     </div>
   </template>
   
   <script>
+  import axios from 'axios';
   import FileSaver from "file-saver";
   import * as XLSX from "xlsx";
   export default {
     data() {
       return {
-        showForm: false, // 控制是否显示表单
-        formData: {
-          description: "", // 表单数据
-        },
-        formRules: { // 表单验证规则
-          description: [
-            { required: true, message: "问题描述不能为空", trigger: "blur" },
-          ],
-        },
+        // showForm: false, // 控制是否显示表单
+        // formData: {
+        //   description: "", // 表单数据
+        // },
+        // formRules: { // 表单验证规则
+        //   description: [
+        //     { required: true, message: "问题描述不能为空", trigger: "blur" },
+        //   ],
+        // },
         tableData: [
           {
-            date: "2002-06-28",
-            ID: "2200022758",
-            name: "ZYY",
-            class: "求知三苑",
-            hours: "30",
+            // date: "2002-06-28",
+            // ID: "2200022758",
+            // name: "ZYY",
+            // class: "求知三苑",
+            // hours: "30",
+            stuNum: "",
+            stuName: "",
+            time: "",
           },
         ],
-        state: 0, // 0-未确认，1-已确认，2-有误，目前默认未确认，写了后端以后可以从后端调用相应数据
+        //state: 0, // 0-未确认，1-已确认，2-有误，目前默认未确认，写了后端以后可以从后端调用相应数据
       };
     },
-    
-    methods: {
-      confirm(){
-        this.state = 1;
-        // 将确认信息传送到后台服务器
-        console.log(state);
-        // 在提交成功后进行一些提示或跳转
-        this.$message.success('信息已确认');
-      },
-      report() {
-        this.$refs.form.validate((valid) => {
-          if (valid) {
-            // 表单验证通过，提交表单数据
-            console.log(this.formData);
-            this.state = 2;
-            this.$message.success("信息已上报，请等待老师与您联系");
-            this.showForm = false; // 隐藏表单
-          } else {
-            // 表单验证不通过，提示错误信息
-            this.$message.error("备注不能为空");
-          }
-        });
-      }
+    mounted() {
+      this.getStudentInfo();
     },
+    methods: {
+      getStudentInfo() {
+      const stuId = 1; // 你需要替换为实际的学生ID
+      axios
+        .post(`http://localhost:28080/api/volunteer/${stuId}/get-volunteer-info`)
+        .then(response => {
+          this.tableData = [response.data.data];
+        })
+        .catch(error => {
+          console.error('获取学生信息失败', error);
+        });
+      },
+      // confirm(){
+      //   this.state = 1;
+      //   // 将确认信息传送到后台服务器
+      //   console.log(state);
+      //   // 在提交成功后进行一些提示或跳转
+      //   this.$message.success('信息已确认');
+      // },
+      // report() {
+      //   this.$refs.form.validate((valid) => {
+      //     if (valid) {
+      //       // 表单验证通过，提交表单数据
+      //       console.log(this.formData);
+      //       this.state = 2;
+      //       this.$message.success("信息已上报，请等待老师与您联系");
+      //       this.showForm = false; // 隐藏表单
+      //     } else {
+      //       // 表单验证不通过，提示错误信息
+      //       this.$message.error("备注不能为空");
+      //     }
+      //   });
+      // }
+    },
+    
   };
   </script>
   
