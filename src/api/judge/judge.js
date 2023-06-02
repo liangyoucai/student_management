9bb3152cb0275f7a6b14efb6c99aee04c79fab12
dumplant <2437789306@qq.com>
2023-06-02 16:24:46 +0800

    【科研、个人】评委axios请求封装在api中

---
+++ src/api/judge/judge.js
@@ -0,0 +1,37 @@
+import request from '@/utils/request';
+
+export default {
+  /**
+   * 评委查看所有summary列表
+   */
+  getSummary() {
+    return request({
+      url: 'api/summary/selectAllList',
+      method: 'get',
+    });
+  },
+
+  /**
+   * 评委查看所有学生填写的${param}表
+   */
+  getSheet(param) {
+    return request({
+      url: `api/${param}/getAllList`,
+      method: 'get',
+    });
+  },
+
+  /**
+   * 评委上传成绩
+   */
+  importScore(data) {
+    return request({
+      url: '/api/summary/import',
+      method: 'post',
+      data: data,
+      headers: {
+        'Content-Type': 'application/json;',
+      },
+    });
+  },
+};
