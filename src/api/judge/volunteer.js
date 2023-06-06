753162399b3377ac4fa52f16705a78eac67a1b81
dumplant <2437789306@qq.com>
2023-06-06 12:43:11 +0800

    删除{stuNum}

--- src/api/judge/volunteer.js
+++ src/api/judge/volunteer.js
@@ -1,33 +1,32 @@
-import request from '@/utils/request'
+import request from '@/utils/request';
 
 export default {
-
-    /**
+  /**
    * 发送查看所有学生志愿服务时长列表请求
-   * @param {*} param 
-   * @returns 
+   * @param {*} param
+   * @returns
    */
-    getList(param) {
-    //   返回请求 
-      return request({
-        url: '/api/volunteer/selectListAll',
-        method: 'post',
-        data: param
-      })
-    },
+  getList(param) {
+    //   返回请求
+    return request({
+      url: '/api/volunteer/selectListAll',
+      method: 'post',
+      data: param,
+    });
+  },
 
-     /**
+  /**
    * 发送志愿服务时长打分请求
-   * @param {*} param 
-   * @returns 
+   * @param {*} param
+   * @returns
    */
-     updateScore(param) {
-      const { stuNum, score } = param; // 提取 stuNum 和 score 属性
-      // 返回请求
-      return request({
-        url: `/api/volunteer/{stuNum}/update-score`,
-        method: 'post',
-        data: { stuNum, score } // 传递 stuNum 和 score 属性
-      });
-    }
-}
+  updateScore(param) {
+    const { stuNum, score } = param; // 提取 stuNum 和 score 属性
+    // 返回请求
+    return request({
+      url: `/api/volunteer/update-score`,
+      method: 'post',
+      data: { stuNum, score }, // 传递 stuNum 和 score 属性
+    });
+  },
+};
