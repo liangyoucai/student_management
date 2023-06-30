d56285512b06dd41146cc33ecf72e30ba1525a56
dumplant <2437789306@qq.com>
2023-06-30 22:19:33 +0800

    添加【学生预览】

--- src/api/student/student.js
+++ src/api/student/student.js
@@ -27,4 +27,15 @@ export default {
       data,
     });
   },
+  /**
+   * 预览学生证明材料
+   */
+  previewFile(subject) {
+    return request({
+      url: `/api/pdf/student/preview`,
+      method: 'post',
+      data: subject.toString(), // 对URLSearchParams对象进行编码
+      responseType: 'arraybuffer',
+    });
+  },
 };
