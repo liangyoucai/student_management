866e062b9a1746c536e05f9708b6d54b5fc426a9
yaoyaolili <104288477+yaoyaolili@users.noreply.github.com>
2023-07-01 13:17:25 +0800

    学生社会实践材料上传和预览

--- src/views/students/SocialUpload.vue
+++ src/views/students/SocialUpload.vue
@@ -2,6 +2,29 @@
   <div class="my-form">
     <h1 class="title">学生社会实践情况 - 在线填写</h1>
     <el-divider></el-divider>
+    <el-row type="flex" justify="center">
+      <el-col :span="24">
+        <h4 style="text-align: center;">证明材料（请将所有证明材料放在一个pdf文件上传）</h4>
+      </el-col>
+    </el-row>
+
+    <el-row type="flex" justify="center">
+      <el-col :span="8" style="display: flex; justify-content: center;">
+        <div>
+          <stuImportPdfButton @click="openImportDialog" subject="practice"></stuImportPdfButton>
+        </div>
+      </el-col>
+    </el-row>
+
+    <el-row type="flex" justify="center">
+      <el-col :span="8" style="display: flex; justify-content: center;">
+        <div>
+          <preview subject="practice"></preview>
+        </div>
+      </el-col>
+    </el-row>
+
+
     <el-form :model="form" label-width="120px" ref="form" >
       <div v-for="(project, index) in form.research" :key="project.key">
         <el-row type="flex">
@@ -67,29 +90,8 @@
               <el-date-picker v-model="form.research[index].time" type="monthrange" range-separator="至" start-placeholder="开始时间" end-placeholder="结束时间">
               </el-date-picker>
             </el-form-item>
-<!--            <el-form-item label="时长" :prop="'research.'+index+'.time'" :rules="{-->
-<!--                            required: true, message: '该项不能为空', trigger: 'blur'-->
-<!--                        }">-->
-<!--              <el-input placeholder="请输入社会实践时长" v-model="form.research[index].time"></el-input>-->
-<!--            </el-form-item>-->
           </el-col>
 
-        </el-row>
-
-        <!-- 证明材料上传 -->
-        <el-row type="flex">
-          <el-row>
-            <el-col :span="24">
-              <h4 style="float: left;">{{ '参与的社会实践项目' + (index + 1) + '证明材料' }}</h4>
-            </el-col>
-          </el-row>
-        </el-row>
-        <el-row>
-          <el-row>
-            <el-col :span="24">
-              <stuImportPdfButton @click="openImportDialog" subject="practice"></stuImportPdfButton>
-            </el-col>
-          </el-row>
         </el-row>
 
       </div>
@@ -105,11 +107,13 @@
 <script>
 import axios from 'axios';
 import user from '@/api/auth/user';
-import student from '@/api/student/student'
-import stuImportPdfButton from "@/components/stuImportPdfButton.vue";
+import student from '@/api/student/student';
+import preview from '@/components/preview.vue'
+import stuImportPdfButton from '@/components/stuImportPdfButton.vue';
 export default {
   components: {
-    stuImportPdfButton
+    stuImportPdfButton,
+    preview
   },
   data() {
     return {
@@ -119,46 +123,17 @@ export default {
             name: '',
             manager: '',
             organization: '',
-            content: '',
-            // level: '',
+            content:'',
             time: '',
             achievements: ''
           }
         ]
       },
-      // levels: ['校级', '市级', '省级', '国家级'],
       username: '',
       num: '',
     };
   },
-  // name: 'PersonalSummaryForm',
-  // data() {
-  //   return {
-  //     form: {
-  //       research: [
-  //         {
-  //           name: '',
-  //           manager: '',
-  //           organization: '',
-  //          // level: '',
-  //           content:'',
-  //           time: '',
-  //           achievements: ''
-  //         }
-  //       ]
-  //     },
-  //    // levels: ['校级', '市级', '省级', '国家级'],
-  //     username: '',
-  //     num: '',
-  //   };
-  // },
-  // mounted() {
-  //     // 当页面被调用，立刻调用该方法，获得的username直接赋值给this对象
-  //     // user.getInfo(this.role).then((res) => {
-  //     //     this.name = res.data.name;
-  //     //     this.num = res.data.num;
-  //     //     this.id = res.data.id;
-  // },
+
   mounted() {
     // 当页面被调用，立刻调用该方法，获得的username直接赋值给this对象
     user.getInfo(this.role).then((res) => {
@@ -189,13 +164,7 @@ export default {
               "constitution": item.organization,
               "director": item.manager,
               "content":item.content,
-              //"level": this.levels.indexOf(item.level),
               "result": item.achievements,
-              // "stuId": 1,
-              // "stuName": '张三',
-              // "stuNum": 2200022001,
-              // "createUserId": 0,
-              // "updateUserId": 0,
               "time": dateRangeString,
               "title": item.name
             })
@@ -224,7 +193,6 @@ export default {
         name: '',
         manager: '',
         organization: '',
-       // level: '',
         content: '',
         time: '',
         achievements: '',
@@ -243,220 +211,8 @@ export default {
   },
 };
 </script>
-<!--<script>-->
-<!--import axios from "axios";-->
-<!--import user from "@/api/auth/user";-->
 
-<!--export default {-->
-<!--  name: 'PersonalSummaryForm',-->
-<!--  data() {-->
-<!--    return {-->
-<!--      form: {-->
-<!--        research: [-->
-<!--          {-->
 
-<!--            username: '',-->
-<!--            num: '',-->
-<!--            name: '',-->
-<!--            manager:'',-->
-<!--            organization:'',-->
-<!--            content: '',-->
-<!--            achievements: '',-->
-<!--            time: ''-->
-
-<!--          }-->
-<!--        ]-->
-<!--      },-->
-<!--      rules: {-->
-
-<!--        name: [-->
-<!--          {required: true, message: '该项不能为空', trigger: 'blur'},-->
-<!--        ],-->
-<!--        manager: [-->
-<!--          {required: true, message: '该项不能为空', trigger: 'blur'},-->
-<!--        ],-->
-<!--        organization: [-->
-<!--          {required: true, message: '该项不能为空', trigger: 'blur'},-->
-<!--        ],-->
-<!--        content: [-->
-<!--          {required: true, message: '该项不能为空', trigger: 'blur'},-->
-<!--        ],-->
-<!--        time: [-->
-<!--          {required: true, message: '该项不能为空', trigger: 'blur'},-->
-<!--        ],-->
-<!--        achievements: [-->
-<!--          {required: true, message: '该项不能为空', trigger: 'blur'},-->
-<!--        ],-->
-<!--      }-->
-<!--    };-->
-<!--  },-->
-<!--  mounted() {-->
-<!--    //console.log("asdasdas")-->
-<!--    // 当页面被调用，立刻调用该方法，获得的username直接赋值给this对象-->
-<!--    user.getInfo(this.role).then((res) => {-->
-<!--      this.username = res.data.username;-->
-<!--      this.num = res.data.num-->
-<!--     // console.log(this.num)-->
-<!--    });-->
-<!--  },-->
-<!--  methods: {-->
-<!--    submitForm() {-->
-<!--      this.$refs.form.validate((valid) => {-->
-<!--        if (valid) {-->
-<!--          let data = []-->
-<!--          for (let item of this.form.research) {-->
-<!--          //  let dateStringArray = []-->
-
-<!--           // console.log(this.num)-->
-<!--           // const start = dateStringArray[0];-->
-<!--          //  const end = dateStringArray[dateStringArray.length - 1];-->
-<!--          //  const dateRangeString = start + ' ~ ' + end;-->
-<!--            data.push({-->
-<!--              "constitution": item.organization,-->
-<!--              "director": item.manager,-->
-<!--             // "level": this.levels.indexOf(item.level),-->
-<!--              "content":item.content,-->
-<!--              "result": item.achievements,-->
-<!--              "score": 0,-->
-<!--              "status": 1,-->
-<!--              "stuId": 72,-->
-<!--              "stuName": this.username,-->
-<!--              "stuNum": this.num,-->
-<!--              "time": item.time,-->
-<!--              "title": item.name-->
-<!--            })-->
-<!--          }-->
-<!--          console.log(data)-->
-<!--          axios.post("http://localhost:18080/api/practice/import", data, {-->
-<!--                headers: {-->
-<!--                  'Content-Type': 'application/json;'-->
-<!--                }-->
-<!--              }-->
-<!--          )-->
-<!--              .then(res => {-->
-<!--                console.log(res);-->
-<!--                if (res.data.code == 200) {-->
-<!--                  this.$message.success("提交成功")-->
-<!--                }-->
-<!--              });-->
-<!--        } else {-->
-<!--          // 表单校验不通过-->
-<!--          this.$message.error('表单填写不完整或有误，请检查');-->
-<!--          return false;-->
-<!--        }-->
-<!--      });-->
-<!--    },-->
-
-<!--    clearForm() {-->
-<!--      this.$refs.form.resetFields();-->
-<!--    },-->
-<!--    addProject() {-->
-<!--      this.form.research.push({-->
-<!--        name: '',-->
-<!--        manager: '',-->
-<!--        organization: '',-->
-<!--       // level: '',-->
-<!--        content:'',-->
-<!--        time: '',-->
-<!--        achievements: '',-->
-<!--        key: Date.now()-->
-<!--      });-->
-<!--      console.log(this.form);-->
-<!--    },-->
-<!--    removeProject(project) {-->
-<!--      var index = this.form.research.indexOf(project)-->
-<!--      if (index !== -1) {-->
-<!--        this.form.research.splice(index, 1)-->
-<!--      }-->
-<!--      console.log(this.form);-->
-
-<!--    }-->
-<!--  },-->
-<!--  // methods: {-->
-<!--  //   submitForm() {-->
-<!--  //-->
-<!--  //     console.log(this.form.research)-->
-<!--  //-->
-<!--  //     this.$refs.form.validate((valid) => {-->
-<!--  //       if (valid) {-->
-<!--  //         let data = []-->
-<!--  //         // let data=[{-->
-<!--  //         //   "constitution": "北京大学",-->
-<!--  //         //   "content": "hahahhaah",-->
-<!--  //         //   "director": "李四",-->
-<!--  //         //   "result": 100,-->
-<!--  //         //   "score": 8,-->
-<!--  //         //   "status": 1,-->
-<!--  //         //   "stu_id": 1,-->
-<!--  //         //   "stu_name": "张三",-->
-<!--  //         //   "stu_num": 2200022123,-->
-<!--  //         //   "time": 10,-->
-<!--  //         //   "title": "智能餐厅管理系统"-->
-<!--  //         // }]-->
-<!--  //         for (let item of this.form.research) {-->
-<!--  //           data.push({-->
-<!--  //             "constitution": item.organization,-->
-<!--  //             "director": item.manager,-->
-<!--  //             "content": item.content,-->
-<!--  //            // "level": this.levels.indexOf(item.level),-->
-<!--  //             "result": parseInt(item.achievements),-->
-<!--  //             "score": 8 ,-->
-<!--  //             "status": 1,-->
-<!--  //             "stu_id": 328,-->
-<!--  //             "stu_name": "李十",-->
-<!--  //             "stu_num": 1111110,-->
-<!--  //             "time": item.time,-->
-<!--  //             "title": item.name-->
-<!--  //           })-->
-<!--  //-->
-<!--  //          }-->
-<!--  //         console.log(data)-->
-<!--  //-->
-<!--  //         axios.post("http://localhost:18080/api/practice/import", data, {-->
-<!--  //               headers: {-->
-<!--  //                 'Content-Type': 'application/json;'-->
-<!--  //               }-->
-<!--  //             }-->
-<!--  //         )-->
-<!--  //             .then(res => {-->
-<!--  //               console.log(res);-->
-<!--  //               if (res.data.code == 200) {-->
-<!--  //                 this.$message.success("提交成功")-->
-<!--  //               }-->
-<!--  //             });-->
-<!--  //       } else {-->
-<!--  //         // 表单校验不通过-->
-<!--  //         this.$message.error('表单填写不完整或有误，请检查');-->
-<!--  //         return false;-->
-<!--  //       }-->
-<!--  //     });-->
-<!--  //   },-->
-<!--  //   clearForm() {-->
-<!--  //     this.$refs.form.resetFields();-->
-<!--  //   },-->
-<!--  //   addProject() {-->
-<!--  //     this.form.research.push({-->
-<!--  //       name: '',-->
-<!--  //       manager:'',-->
-<!--  //       organization: '',-->
-<!--  //       content: '',-->
-<!--  //       achievements: '',-->
-<!--  //       time: '',-->
-<!--  //       key: Date.now()-->
-<!--  //     });-->
-<!--  //     console.log(this.form);-->
-<!--  //   },-->
-<!--  //   removeProject(project) {-->
-<!--  //     var index = this.form.research.indexOf(project)-->
-<!--  //     if (index !== -1) {-->
-<!--  //       this.form.research.splice(index, 1)-->
-<!--  //     }-->
-<!--  //     console.log(this.form);-->
-<!--  //-->
-<!--  //   }-->
-<!--  // },-->
-<!--};-->
-<!--</script>-->
 
 <style scoped>
 .title {
