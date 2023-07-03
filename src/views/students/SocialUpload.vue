2c48b373e95a151fa0e297c5606e9c66202d99cd
yaoyaolili <104288477+yaoyaolili@users.noreply.github.com>
2023-07-03 15:39:34 +0800

    学生查看

--- src/views/students/SocialUpload.vue
+++ src/views/students/SocialUpload.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="my-form">
-    <h1 class="title">学生社会实践情况 - 在线填写</h1>
+    <h1 class="title">学生骨干服务情况 - 在线填写</h1>
     <el-divider></el-divider>
+
+
+    <el-row type="flex">
+      <el-row>
+        <el-col :span="24">
+          <h4 style="float: left;">已提交内容</h4>
+        </el-col>
+      </el-row>
+    </el-row>
+    <el-row>
+      <el-col>
+        <el-button style="float: left;" @click="toggleSubmittedData" type="text">{{ showSubmittedData ? "收起" : "展开" }}</el-button>
+      </el-col>
+    </el-row>
+    <!-- 已提交内容总览表 -->
+    <submitted-data-table v-if="showSubmittedData" :submittedData="submittedData.slice().sort((a, b) => a.id - b.id)">
+
+      <el-table-column label="岗位名称" prop="title"></el-table-column>
+      <el-table-column label="岗位负责人" prop="director"></el-table-column>
+      <el-table-column label="岗位负责机构" prop="constitution"></el-table-column>
+      <el-table-column label="岗位职责" prop="content"></el-table-column>
+      <el-table-column label="参与时间" prop="time"></el-table-column>
+      <el-table-column label="在岗期间成果" prop="result"></el-table-column>
+
+    </submitted-data-table>
+
     <el-row type="flex" justify="center">
       <el-col :span="24">
         <h4 style="text-align: center;">证明材料（请将所有证明材料放在一个pdf文件上传）</h4>
@@ -11,7 +37,7 @@
     <el-row type="flex" justify="center">
       <el-col :span="8" style="display: flex; justify-content: center;">
         <div>
-          <stuImportPdfButton @click="openImportDialog" subject="practice"></stuImportPdfButton>
+          <stuImportPdfButton @click="openImportDialog" subject="service"></stuImportPdfButton>
         </div>
       </el-col>
     </el-row>
@@ -19,17 +45,18 @@
     <el-row type="flex" justify="center">
       <el-col :span="8" style="display: flex; justify-content: center;">
         <div>
-          <preview subject="practice"></preview>
+          <preview subject="service"></preview>
         </div>
       </el-col>
     </el-row>
+
 
 
     <el-form :model="form" label-width="120px" ref="form" >
       <div v-for="(project, index) in form.research" :key="project.key">
         <el-row type="flex">
           <el-col :span="22">
-            <h4 style="float: left;">{{ '参与的社会实践项目' + (index + 1) }} </h4>
+            <h4 style="float: left;">{{ '参与的服务项目' + (index + 1) }}</h4>
           </el-col>
           <!-- 删除按钮 -->
           <el-col :span="2" style="display:flex;justify-content:flex-end;align-items:center;">
@@ -41,37 +68,34 @@
             <el-form-item label="名称" :prop="'research.'+index+'.name'" :rules="{
                             required: true, message: '该项不能为空', trigger: 'blur'
                         }">
-              <el-input placeholder="请输入社会实践名称" v-model="form.research[index].name" >
-              </el-input>
+              <el-input placeholder="请输入服务岗位名称" v-model="form.research[index].name"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="20">
-            <el-form-item label="负责人" :prop="'research.'+index+'.manager'" :rules="{
+            <el-form-item label="岗位负责人" :prop="'research.'+index+'.manager'" :rules="{
                             required: true, message: '该项不能为空', trigger: 'blur'
                         }">
-              <el-input placeholder="请输入社会实践负责人" v-model="form.research[index].manager">
-              </el-input>
+              <el-input placeholder="请输入服务岗位负责人" v-model="form.research[index].manager"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="20">
-            <el-form-item label="负责单位" :prop="'research.'+index+'.organization'" :rules="{
+            <el-form-item label="岗位负责单位" :prop="'research.'+index+'.organization'" :rules="{
                             required: true, message: '该项不能为空', trigger: 'blur'
                         }">
-              <el-input placeholder="请输入社会实践负责单位" v-model="form.research[index].organization">
-              </el-input>
+              <el-input placeholder="请输入服务岗位负责单位" v-model="form.research[index].organization"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="20">
-            <el-form-item label="内容" ::prop="'research.'+index+'.content'" :rules="{
+            <el-form-item label="岗位职责" :prop="'research.'+index+'.content'" :rules="{
                             required: true, message: '该项不能为空', trigger: 'blur'
                         }">
-              <el-input placeholder="请输入社会实践内容" v-model="form.research[index].content"></el-input>
+              <el-input placeholder="请输入岗位职责" v-model="form.research[index].content"></el-input>
             </el-form-item>
           </el-col>
         </el-row>
@@ -80,7 +104,7 @@
             <el-form-item label="成果" :prop="'research.'+index+'.achievements'" :rules="{
                             required: true, message: '该项不能为空', trigger: 'blur'
                         }">
-              <el-input placeholder="请输入社会实践成果" v-model="form.research[index].achievements"></el-input>
+              <el-input placeholder="请输入在岗期间成果" v-model="form.research[index].achievements"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="20">
@@ -90,13 +114,13 @@
               <el-date-picker v-model="form.research[index].time" type="monthrange" range-separator="至" start-placeholder="开始时间" end-placeholder="结束时间">
               </el-date-picker>
             </el-form-item>
-          </el-col>
 
+          </el-col>
         </el-row>
 
       </div>
       <el-form-item style="margin-top: 20px">
-        <el-button type="warning" @click="addProject">增加社会实践项目</el-button>
+        <el-button type="warning" @click="addProject">增加服务项目</el-button>
         <el-button type="primary" @click="submitForm">提交</el-button>
         <el-button @click="clearForm">重置</el-button>
       </el-form-item>
@@ -110,10 +134,12 @@ import user from '@/api/auth/user';
 import student from '@/api/student/student';
 import preview from '@/components/preview.vue'
 import stuImportPdfButton from '@/components/stuImportPdfButton.vue';
+import submittedDataTable from '@/components/SubmittedDataTable.vue'
 export default {
   components: {
     stuImportPdfButton,
-    preview
+    preview,
+    submittedDataTable
   },
   data() {
     return {
@@ -131,18 +157,32 @@ export default {
       },
       username: '',
       num: '',
+      submittedData: []
     };
   },
-
   mounted() {
-    // 当页面被调用，立刻调用该方法，获得的username直接赋值给this对象
     user.getInfo(this.role).then((res) => {
       this.username = res.data.username;
       this.num = res.data.num
       console.log(res.data)
+      student.reviewMyList('service', this.num).then(res => {
+        this.submittedData = res.data;
+      })
     });
+
   },
+  // mounted() {
+  //   // 当页面被调用，立刻调用该方法，获得的username直接赋值给this对象
+  //   user.getInfo(this.role).then((res) => {
+  //     this.username = res.data.username;
+  //     this.num = res.data.num
+  //     console.log(res.data)
+  //   });
+  // },
   methods: {
+    toggleSubmittedData() {
+      this.showSubmittedData = !this.showSubmittedData;
+    },
     openImportDialog() {
       this.$refs.ChildButton.openImportDialog();
     },
@@ -170,7 +210,7 @@ export default {
             })
           }
           console.log(data)
-          student.import('practice', data)
+          student.import('service', data)
               .then(res => {
                 console.log(res);
                 if (res.code == 200) {
@@ -211,8 +251,6 @@ export default {
   },
 };
 </script>
-
-
 
 <style scoped>
 .title {
